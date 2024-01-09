8c4e652d58f0a69890acb15877a6f545671bd6e7
megas-mdn <mdn.melqui@gmail.com>
2024-01-09 00:51:35 -0300

    feat: Adding excel data file

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const xlsx = require('xlsx');
 const path = require('path');
+const fs = require('fs/promises');
 const assets = require('./assets/__assets.js');
 // create a xlsx file
 const workbook = xlsx.utils.book_new();
 
-let student_data = [
-  {
-    Student: 'Nikhil',
-    Age: 22,
-    Branch: 'ISE',
-    Marks: 70,
-  },
-  {
-    Student: 'Amitha',
-    Age: 21,
-    Branch: 'EC',
-    Marks: 80,
-  },
-];
+const genObj = (ticket) => ({
+  ativo: ticket,
+  'Quantidade de Compra': `=RTD("tryd.rtdserver";;"COT";"${ticket}";"QtdCpa")`,
+  Compra: `=RTD("tryd.rtdserver";;"COT";"${ticket}";"PrcCpa")`,
+  Ultimo: `=RTD("tryd.rtdserver";;"COT";"${ticket}";"Ult")`,
+  Venda: `=RTD("tryd.rtdserver";;"COT";"${ticket}";"PrcVda")`,
+  'Quantidade de Venda': `=RTD("tryd.rtdserver";;"COT";"${ticket}";"QtdVda")`,
+  Negocios: `=RTD("tryd.rtdserver";;"COT";"${ticket}";"QtdNeg")`,
+});
 
-const ws = xlsx.utils.json_to_sheet(student_data);
+fs.readFile(path.resolve(assets, 'dataTickets.json'), 'utf8').then((data) => {
+  const tickets = JSON.parse(data);
+  const ws = xlsx.utils.json_to_sheet(tickets);
 
-xlsx.utils.book_append_sheet(workbook, ws, 'Sheet1');
+  xlsx.utils.book_append_sheet(workbook, ws, 'Sheet1');
 
-xlsx.writeFile(workbook, path.resolve(assets, 'data.xlsx'));
+  xlsx.writeFile(workbook, path.resolve(assets, 'data.xlsx'));
+  console.log('Done ---');
+});
